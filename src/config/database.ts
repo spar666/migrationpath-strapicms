c093c0439f5d19c5e6e99fdaadcaf22c429dcaf9
ashishpokhrel123 <aashishpokhrel146@gmail.com>
2026-06-09 21:33:51 +0545

    remove better slite

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,11 +1,13 @@
 export default ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: env('DATABASE_CLIENT', 'postgres'),
     connection: {
       connectionString: env('DATABASE_URL'),
-      ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
-      },
+      ssl: env.bool('DATABASE_SSL', false)
+        ? {
+            rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
+          }
+        : false,
     },
     pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
