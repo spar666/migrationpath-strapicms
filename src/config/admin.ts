7088061367fb3a79ce83745f7fd03fa233a42dc2
ashishpokhrel123 <aashishpokhrel146@gmail.com>
2026-06-09 23:35:41 +0545

    fix: hardcode all admin config secrets

--- src/config/admin.ts
+++ src/config/admin.ts
@@ -2,18 +2,18 @@ import type { Core } from '@strapi/strapi';
 // test
 const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Admin => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'KWDqyf/G34wWlPycSJVabg=='),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'Ad9d69K7nODI/qv5KWF+iQ=='),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', '/WiaRIk9CCzXr1dlhh7nPA=='),
     },
   },
   secrets: {
-    encryptionKey: env('ENCRYPTION_KEY'),
+    encryptionKey: env('ENCRYPTION_KEY', '84lKFcy6HiGeRzPhpj2hRw=='),
   },
   flags: {
     nps: env.bool('FLAG_NPS', true),
