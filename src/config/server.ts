b24aa6693573f6c31639efe8b557a4a201358e63
ashishpokhrel123 <aashishpokhrel146@gmail.com>
2026-06-10 10:31:48 +0545

    fix: revert to APP_KEYS env var for Strapi Cloud compatibility

--- src/config/server.ts
+++ src/config/server.ts
@@ -6,7 +6,7 @@ const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Server =>
     port: env.int('PORT', 1337),
     url: env('PUBLIC_URL', 'http://localhost:1337'),
     app: {
-      keys: env.array('PRODUCTION_APP_KEY', ['zPs56quTCRHK9Qyv5pIEog==', '7hzrbxT9sG4Ki9w00PerOg==', 'xLrViUfkBckbaVDoI+MJsw==', 'bkhOqhIG2+rnILZezFaYtw==']),
+      keys: env.array('APP_KEYS', ['zPs56quTCRHK9Qyv5pIEog==', '7hzrbxT9sG4Ki9w00PerOg==', 'xLrViUfkBckbaVDoI+MJsw==', 'bkhOqhIG2+rnILZezFaYtw==']),
     },
   };
 };
