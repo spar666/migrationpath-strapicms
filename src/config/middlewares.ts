bec116c8b65f9941d0e7aed0d75f0d99f40972ce
ashishpokhrel123 <aashishpokhrel146@gmail.com>
2026-06-10 21:00:03 +0545

    fix

--- src/config/middlewares.ts
+++ src/config/middlewares.ts
@@ -1,12 +1,61 @@
-export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+export const config = {
+  resolve: {
+    'strapi::errors': {
+      enabled: true,
+    },
+    'strapi::security': {
+      enabled: true,
+      config: {
+        contentSecurityPolicy: {
+          useDefaults: true,
+          directives: {
+            'connect-src': ["'self'", 'https:'],
+            'img-src': ["'self'", 'data:', 'blob:', 'https:'],
+            'media-src': ["'self'", 'data:', 'blob:', 'https:'],
+            upgradeInsecureRequests: null,
+          },
+        },
+      },
+    },
+    'strapi::cors': {
+      enabled: true,
+      config: {
+        origin: ['localhost', 'http://localhost:3000', 'http://localhost:8000'],
+        credentials: true,
+        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      },
+    },
+    'strapi::poweredBy': {
+      enabled: true,
+    },
+    'strapi::logger': {
+      enabled: true,
+    },
+    'strapi::query': {
+      enabled: true,
+    },
+    'strapi::body': {
+      enabled: true,
+      config: {
+        formLimit: '256mb',
+        jsonLimit: '256mb',
+        textLimit: '256mb',
+      },
+    },
+    'strapi::session': {
+      enabled: true,
+      config: {
+        defaultPath: '/admin',
+        sameSite: 'lax',
+      },
+    },
+    'strapi::favicon': {
+      enabled: true,
+    },
+    'strapi::public': {
+      enabled: true,
+    },
+  },
+};
+
+export default config;
