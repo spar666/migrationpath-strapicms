7fb49607e64a3eee6302d9dbe303dd28dc7ba85f
ashishpokhrel123 <aashishpokhrel146@gmail.com>
2026-06-10 09:06:48 +0545

    remove better slite

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,12 +1,14 @@
 import type { Core } from '@strapi/strapi';
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Server => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'http://localhost:1337'),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-});
+const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Server => {
+  return {
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1337),
+    url: env('PUBLIC_URL', 'http://localhost:1337'),
+    app: {
+      keys: env.array('APP_KEYS', ['zPs56quTCRHK9Qyv5pIEog==', '7hzrbxT9sG4Ki9w00PerOg==', 'xLrViUfkBckbaVDoI+MJsw==', 'bkhOqhIG2+rnILZezFaYtw==']),
+    },
+  };
+};
 
 export default config;
